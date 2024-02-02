a966991ffd98773c5a1edb50e5a04c4493e835ec
Sai Teja <saiteja.khf6#@gmail.com>
2024-02-01 21:01:06 -0500

    changing resume

--- pages.js
+++ pages.js
@@ -1,6 +1,6 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/BunnyTeja/BunnyTeja.github.io.git";
+const repoURL = "https://github.com/BunnyTeja/Teja.github.io.git";
 
 ghpages.publish(
   pathname,
