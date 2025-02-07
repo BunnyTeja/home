ebc39e4d6939c4d17e64cc9918efc3df4164ceb7
SaiPaladi7 <sai.paladi765@gmail.com>
2025-02-07 15:25:47 -0500

    test

--- src/components/home/MainBody.jsx
+++ src/components/home/MainBody.jsx
@@ -44,7 +44,7 @@ const MainBody = React.forwardRef(
             role="button"
             aria-label="Learn more about me"
           >
-            More about meeee
+            More about me
           </a>
         </Container>
       </Jumbotron>
